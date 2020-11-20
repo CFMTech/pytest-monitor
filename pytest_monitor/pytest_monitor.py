9c0919fb8975c26d4e843d1b6d9246be68bf4746
Jean-Sebastien Dieu <60646752+js-dieu@users.noreply.github.com>
2020-11-20 10:41:33 +0100

    #26 - Fix a bug that causes output to be repeated several times when invoking pytest with -s flag (#28)
    #26 - Fix a bug that causes output to be repeated several times when invoking pytest with '-s' flag. The requirement on memory_profiler has been upgraded in order to enforce only one iteration for collecting memory usage

--- pytest_monitor/pytest_monitor.py
+++ pytest_monitor/pytest_monitor.py
@@ -118,12 +118,12 @@ def pytest_runtest_call(item):
     setattr(item, 'monitor_component', getattr(item.module, 'pytest_monitor_component', ''))
 
 
+@pytest.hookimpl
 def pytest_pyfunc_call(pyfuncitem):
     """
     Core sniffer logic. We encapsulate the test function in a sniffer function to collect
     memory results.
     """
-
     def wrapped_function():
         try:
             funcargs = pyfuncitem.funcargs
@@ -135,10 +135,12 @@ def pytest_pyfunc_call(pyfuncitem):
             return e
 
     def prof():
-        m = memory_profiler.memory_usage((wrapped_function, ()), max_usage=True, retval=True)
+        m = memory_profiler.memory_usage((wrapped_function, ()),
+                                         max_iterations=1, max_usage=True, retval=True)
         if isinstance(m[1], BaseException):  # Do we have any outcome?
             raise m[1]
-        setattr(pyfuncitem, 'mem_usage', m[0])
+        memuse = m[0][0] if type(m[0]) is list else m[0]
+        setattr(pyfuncitem, 'mem_usage', memuse)
         setattr(pyfuncitem, 'monitor_results', True)
     prof()
     return True
